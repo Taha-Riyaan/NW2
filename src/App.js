f0328ea45883afc3770965cd13344d32ba7fcc25
Taha-Riyaan <tahariyaan@gmail.com>
2024-07-22 00:55:00 -0400

    hasrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Types from './components/Types'
